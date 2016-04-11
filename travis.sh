bbc8647847b691f67eea4898e8d23eb55b379cf8
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-04-11 17:42:36 +0200

    Fix Travis configuration

--- travis.sh
+++ travis.sh
@@ -2,38 +2,41 @@
 
 set -euo pipefail
 
-function strongEcho {
-  echo ""
-  echo "================ $1 ================="
-}
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo '======= Build and analyze master'
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  strongEcho 'Build and analyze commit in master'
-  mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
-    -Pcoverage-per-test \
-    -Dmaven.test.redirectTestOutputToFile=false \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.login=$SONAR_TOKEN \
-    -B -e -V
+  export MAVEN_OPTS="-Xmx1G -Xms128m"
+  mvn org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
+      -Pcoverage-per-test \
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN \
+      -B -e -V
 
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  strongEcho 'Build and analyze pull request'
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+  echo '======= Build and analyze pull request'
+
+  # No need for Maven phase "install" as the generated JAR files do not need to be installed
+  # in Maven local repository. Phase "verify" is enough.
+
+  export MAVEN_OPTS="-Xmx1G -Xms128m"
   mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
-    -Pcoverage-per-test \
-    -Dmaven.test.redirectTestOutputToFile=false \
-    -Dsonar.analysis.mode=issues \
-    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-    -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-    -Dsonar.github.oauth=$GITHUB_TOKEN \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.login=$SONAR_TOKEN \
-    -B -e -V
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN \
+      -B -e -V
 
 else
-  strongEcho 'Build, no analysis'
-  # Build branch, without any analysis
+  echo '======= Build, no analysis'
 
-  # No need for Maven goal "install" as the generated JAR file does not need to be installed
-  # in Maven local repository
-  mvn verify -Dmaven.test.redirectTestOutputToFile=false -B -e -V
+  # No need for Maven phase "install" as the generated JAR files do not need to be installed
+  # in Maven local repository. Phase "verify" is enough.
+
+  mvn verify \
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -B -e -V
 fi
