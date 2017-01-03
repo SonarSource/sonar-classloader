db1259c7850a2807a26be3fc168fdd0d40797237
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-01-03 15:52:49 +0100

    Upgrade travis-utils

--- travis.sh
+++ travis.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+function configureTravis {
+  mkdir ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+configureTravis
+. installJDK8
+
 if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo '======= Build and analyze master'
 
