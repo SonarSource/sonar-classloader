374b27e9e9f71a22ef8e7f115d764877e1006ba3
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-01-03 15:56:46 +0100

    Use Travis script regular_mvn_build_deploy_analyze

--- travis.sh
+++ travis.sh
@@ -10,41 +10,4 @@ function configureTravis {
 configureTravis
 . installJDK8
 
-if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo '======= Build and analyze master'
-
-  export MAVEN_OPTS="-Xmx1G -Xms128m"
-  mvn org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
-      -Pcoverage-per-test \
-      -Dmaven.test.redirectTestOutputToFile=false \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_TOKEN \
-      -B -e -V
-
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
-  echo '======= Build and analyze pull request'
-
-  # No need for Maven phase "install" as the generated JAR files do not need to be installed
-  # in Maven local repository. Phase "verify" is enough.
-
-  export MAVEN_OPTS="-Xmx1G -Xms128m"
-  mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
-      -Dmaven.test.redirectTestOutputToFile=false \
-      -Dsonar.analysis.mode=issues \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-      -Dsonar.github.oauth=$GITHUB_TOKEN \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_TOKEN \
-      -B -e -V
-
-else
-  echo '======= Build, no analysis'
-
-  # No need for Maven phase "install" as the generated JAR files do not need to be installed
-  # in Maven local repository. Phase "verify" is enough.
-
-  mvn verify \
-      -Dmaven.test.redirectTestOutputToFile=false \
-      -B -e -V
-fi
+regular_mvn_build_deploy_analyze
